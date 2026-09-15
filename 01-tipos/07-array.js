dd2a8cd96cfe8ade14c99e1d483fdc71be452170
TomasC5 <tomas.azumas@gmail.com>
2026-09-15 20:52:02 -0300

    objetos

---
+++ 01-tipos/07-array.js
@@ -0,0 +1,10 @@
+let animales = ['chancho', 'caballo'];
+console.log(animales[1]);
+
+animales[2] = 'gato';
+console.log(animales);
+
+//array son objetos
+//si pongo [10] se generan espacios vacios en el array
+
+console.log(animales.length);
