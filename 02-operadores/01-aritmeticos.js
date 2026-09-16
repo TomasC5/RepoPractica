4b363af192482d48cbe6113562cc548c06614cc3
TomasC5 <tomas.azumas@gmail.com>
2026-09-16 19:36:38 -0300

    cosas

---
+++ 02-operadores/01-aritmeticos.js
@@ -0,0 +1,21 @@
+let a = 5;
+let b = 7;
+
+// console.log(a + b); // adicion
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+
+// incrementar
+
+console.log(++a); //incrementa en 1
+console.log(a++); //imprime A y despues suma
+console.log(a);
+
+// decrementar
+
+console.log(--a);
+console.log(a--);
+console.log(a);
---
+++ 02-operadores/03-comparacion.js
@@ -0,0 +1,11 @@
+let a = 10;
+console.log(a > 5);
+console.log(a >= 5);
+console.log(a < 5);
+console.log(a <= 5);
+
+console.log(a == 10);
+console.log(a != 10);
+console.log(a == "10"); //solo pregunta por el valor, no el tipo de dato
+console.log(a === "10");
+console.log(a !== "10");
